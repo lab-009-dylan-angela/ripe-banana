5ca26a7b6d15e936f7d1a4103e6ef8075abc6857
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-27 17:21:59 -0700

    reviews test with populate data passing, films stopped passing somehow

--- e2e/__tests__/reviews.test.js
+++ e2e/__tests__/reviews.test.js
@@ -17,12 +17,14 @@ function postReviewer(reviewer) {
 
 const film1 = {
   title: 'Beetlejuice',
-  studio: new mongoose.Types.ObjectId,
+  studio: new mongoose.Types.ObjectId(),
   released: 1988,
-  cast: [{
-    role: 'Lydia Deetz',
-    actor: new mongoose.Types.ObjectId
-  }]
+  cast: [
+    {
+      role: 'Lydia Deetz',
+      actor: new mongoose.Types.ObjectId()
+    }
+  ]
 };
 
 function postFilm(film) {
@@ -40,9 +42,9 @@ describe('reviews api', () => {
 
   const data = {
     rating: 5,
-    reviewer: new mongoose.Types.ObjectId,
-    review: 'Tim Burton when he still had original ideas.', 
-    film: new mongoose.Types.ObjectId
+    reviewer: new mongoose.Types.ObjectId(),
+    review: 'Tim Burton when he still had original ideas.',
+    film: new mongoose.Types.ObjectId()
   };
 
   function postReview(review) {
@@ -54,17 +56,16 @@ describe('reviews api', () => {
   }
 
   it('posts a review', () => {
-    return postReview(data)
-      .then(review => {
-        expect(review).toEqual({
-          _id: expect.any(String),
-          __v: 0,
-          rating: 5,
-          reviewer: expect.any(String),
-          review: 'Tim Burton when he still had original ideas.', 
-          film: expect.any(String)
-        });
+    return postReview(data).then(review => {
+      expect(review).toEqual({
+        _id: expect.any(String),
+        __v: 0,
+        rating: 5,
+        reviewer: expect.any(String),
+        review: 'Tim Burton when he still had original ideas.',
+        film: expect.any(String)
       });
+    });
   });
 
   it('gets review by id', () => {
@@ -72,34 +73,48 @@ describe('reviews api', () => {
       data.reviewer = postedReviewer._id;
       return postFilm(film1).then(postedFilm => {
         data.film = postedFilm._id;
-        return postReview(data)
-          .then(review => {
-            return request.get(`/api/reviews/${review._id}`)
-              .expect(200)
-              .then(({ body }) => {
-                expect(body).toMatchInLineSnapshop(
-                  {
-                    _id: expect.any(String),
-                    reviewer: {
-                      _id: expect.any(String)
-                    },
-                    film: {
-                      _id: expect.any(String)
-                    }
+        return postReview(data).then(review => {
+          console.log(review);
+
+          return request
+            .get(`/api/reviews/${review._id}`)
+            .expect(200)
+            .then(({ body }) => {
+              expect(body).toMatchInlineSnapshot(
+                {
+                  _id: expect.any(String),
+                  reviewer: {
+                    _id: expect.any(String)
                   },
-                );
-              });
-          });
+                  film: {
+                    _id: expect.any(String)
+                  }
+                },
+                `
+                Object {
+                  "__v": 0,
+                  "_id": Any<String>,
+                  "film": Object {
+                    "_id": Any<String>,
+                  },
+                  "rating": 5,
+                  "review": "Tim Burton when he still had original ideas.",
+                  "reviewer": Object {
+                    "_id": Any<String>,
+                    "name": "Gene Shalit",
+                  },
+                }
+              `
+              );
+            });
+        });
       });
     });
   });
 
   it('deletes a review', () => {
-    return postReview(data)
-      .then(review => {
-        return request
-          .delete(`/api/reviews/${review._id}`)
-          .expect(200);
-      });
+    return postReview(data).then(review => {
+      return request.delete(`/api/reviews/${review._id}`).expect(200);
+    });
   });
 });
