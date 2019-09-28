5e9eaf17e36ee9afc60a946b3711192e232e5e36
Angela Vawser <amvawser@gmail.com>
2019-09-27 17:42:06 -0700

    fixed prettier errors

--- e2e/__tests__/films.test.js
+++ e2e/__tests__/films.test.js
@@ -1,50 +1,50 @@
-const request = require("../request");
-const db = require("../db");
-const mongoose = require("mongoose");
+const request = require('../request');
+const db = require('../db');
+const mongoose = require('mongoose');
 
 const actor1 = {
-  name: "Winona Ryder",
+  name: 'Winona Ryder',
   dob: 1971,
-  pob: "Winona, Minnesota"
+  pob: 'Winona, Minnesota'
 };
 
 function postActor(actor) {
   return request
-    .post("/api/actors")
+    .post('/api/actors')
     .send(actor)
     .expect(200)
     .then(({ body }) => body);
 }
 
 const studio1 = {
-  name: "Warner Brothers",
+  name: 'Warner Brothers',
   address: {
-    city: "Burbank",
-    state: "California",
-    country: "USA"
+    city: 'Burbank',
+    state: 'California',
+    country: 'USA'
   }
 };
 
 function postStudio(studio) {
   return request
-    .post("/api/studios")
+    .post('/api/studios')
     .send(studio)
     .expect(200)
     .then(({ body }) => body);
 }
 
-describe("films api", () => {
+describe('films api', () => {
   beforeEach(() => {
-    return db.dropCollection("films");
+    return db.dropCollection('films');
   });
 
   const data = {
-    title: "Beetlejuice",
+    title: 'Beetlejuice',
     studio: new mongoose.Types.ObjectId(),
     released: 1988,
     cast: [
       {
-        role: "Lydia Deetz",
+        role: 'Lydia Deetz',
         actor: new mongoose.Types.ObjectId()
       }
     ]
@@ -52,23 +52,23 @@ describe("films api", () => {
 
   function postFilm(film) {
     return request
-      .post("/api/films")
+      .post('/api/films')
       .send(film)
       .expect(200)
       .then(({ body }) => body);
   }
 
-  it("posts a film", () => {
+  it('posts a film', () => {
     return postFilm(data).then(film => {
       expect(film).toEqual({
         _id: expect.any(String),
         __v: 0,
-        title: "Beetlejuice",
+        title: 'Beetlejuice',
         studio: expect.any(String),
         released: 1988,
         cast: [
           {
-            role: "Lydia Deetz",
+            role: 'Lydia Deetz',
             actor: expect.any(String),
             _id: expect.any(String)
           }
@@ -77,7 +77,7 @@ describe("films api", () => {
     });
   });
 
-  it("gets film by id", () => {
+  it('gets film by id', () => {
     return postActor(actor1).then(postedActor => {
       data.cast[0].actor = postedActor._id;
       return postStudio(studio1).then(postedStudio => {
@@ -129,7 +129,7 @@ describe("films api", () => {
     });
   });
 
-  it("deletes a film", () => {
+  it('deletes a film', () => {
     return postFilm(data).then(film => {
       return request.delete(`/api/films/${film._id}`).expect(200);
     });
